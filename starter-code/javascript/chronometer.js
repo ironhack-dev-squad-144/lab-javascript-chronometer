3e6d34f9ac725232b8ef71bbda06ce5e5058b799
Maxence Bouret <maxence.bouret@gmail.com>
2019-06-10 16:57:49 +0100

    Solution

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,10 +1,27 @@
 class Chronometer {
-  constructor() {}
-  // startClick() {}
-  // getMinutes() {}
-  // getSeconds() {}
-  // twoDigitsNumber() {}
-  // stopClick() {}
-  // resetClick() {}
+  constructor() {
+    this.currentTime = 0
+  }
+  startClick() {
+    this.intervalId = setInterval(() => {
+      this.currentTime++
+    }, 1000)
+  }
+  getMinutes() {
+    return Math.floor(this.currentTime / 60)
+  }
+  getSeconds() {
+    return this.currentTime % 60
+  }
+  twoDigitsNumber(n) {
+    if (n < 10) return "0"+n
+    else return n.toString()
+  }
+  stopClick() {
+    clearInterval(this.intervalId)
+  }
+  resetClick() {
+    this.currentTime = 0
+  }
   // splitClick() {}
 }
--- starter-code/javascript/main.js
+++ starter-code/javascript/main.js
@@ -1,60 +1,133 @@
+// var chronometer = {
+//   currentTime: 0,
+//   startClick: function() {
+//     this.intervalId = setInterval(() => {
+//       this.currentTime++
+//     }, 1000)
+//   },
+//   getMinutes: function() {
+//     return Math.floor(this.currentTime / 60)
+//   },
+//   getSeconds: function() {
+//     return this.currentTime % 60
+//   },
+//   twoDigitsNumber: function(n) {
+//     if (n < 10) return "0"+n
+//     else return n.toString()
+//   },
+//   stopClick: function() {
+//     clearInterval(this.intervalId)
+//   },
+//   resetClick: function() {
+//     this.currentTime = 0
+//   }
+// }
+
 var chronometer = new Chronometer();
-var btnLeft     = document.getElementById('btnLeft');
-var btnRight    = document.getElementById('btnRight');
-var minDec      = document.getElementById('minDec');
-var minUni      = document.getElementById('minUni');
-var secDec      = document.getElementById('secDec');
-var secUni      = document.getElementById('secUni');
-var milDec      = document.getElementById('milDec');
-var milUni      = document.getElementById('milUni');
+var $btnLeft    = document.getElementById('btnLeft');
+var $btnRight   = document.getElementById('btnRight');
+var $minDec     = document.getElementById('minDec');
+var $minUni     = document.getElementById('minUni');
+var $secDec     = document.getElementById('secDec');
+var $secUni     = document.getElementById('secUni');
+var $milDec     = document.getElementById('milDec');
+var $milUni     = document.getElementById('milUni');
+var $splitsList = document.querySelector('#splits');
 
-
+// This function is only modifying the DOM
 function printTime() {
-
+  printMinutes()
+  printSeconds()
 }
 
+// This function is only modifying the DOM
 function printMinutes() {
-
+  let stringMin = chronometer.twoDigitsNumber(chronometer.getMinutes())
+  $minDec.innerText = stringMin[0]
+  $minUni.innerText = stringMin[1]
 }
 
+// This function is only modifying the DOM
 function printSeconds() {
-
+  let stringSec = chronometer.twoDigitsNumber(chronometer.getSeconds())
+  $secDec.innerText = stringSec[0]
+  $secUni.innerText = stringSec[1]
 }
 
 function printMilliseconds() {
-
 }
 
-function printSplit() {
-
+// This function is only modifying the DOM
+function printNewSplit() {
+  let stringMin = chronometer.twoDigitsNumber(chronometer.getMinutes())
+  let stringSec = chronometer.twoDigitsNumber(chronometer.getSeconds())
+  // Insert at the end of $splitsList
+  $splitsList.innerHTML += `<li>${stringMin}:${stringSec}</li>`
 }
 
+// This function is only modifying the DOM
 function clearSplits() {
-
+  $splitsList.innerHTML = ""
 }
 
+// This function is only modifying the DOM
 function setStopBtn() {
-
+  $btnLeft.classList.remove('start')
+  $btnLeft.classList.add('stop')
+  $btnLeft.innerText = 'STOP'
 }
 
+// This function is only modifying the DOM
 function setSplitBtn() {
-
+  $btnRight.classList.remove('reset')
+  $btnRight.classList.add('split')
+  $btnRight.innerText = 'SPLIT'
 }
 
+// This function is only modifying the DOM
 function setStartBtn() {
-
+  $btnLeft.classList.add('start')
+  $btnLeft.classList.remove('stop')
+  $btnLeft.innerText = 'START'
 }
 
+// This function is only modifying the DOM
 function setResetBtn() {
-
+  $btnRight.classList.add('reset')
+  $btnRight.classList.remove('split')
+  $btnRight.innerText = 'RESET'
 }
 
 // Start/Stop Button
-btnLeft.addEventListener('click', function () {
-
-});
+let intervalId
+$btnLeft.onclick = function () {
+  // If we click on start
+  if ($btnLeft.innerText === 'START') {
+    chronometer.startClick()
+    intervalId = setInterval(() => {
+      console.log("setInterval")
+      printTime()
+    }, 1000)
+    setStopBtn()
+    setSplitBtn()
+  }
+  else {
+    chronometer.stopClick()
+    clearInterval(intervalId)
+    setStartBtn()
+    setResetBtn()
+  }
+};
 
 // Reset/Split Button
-btnRight.addEventListener('click', function () {
-
-});
+$btnRight.onclick = function () {
+  console.log("Right button clicked", $btnRight.innerText === 'SPLIT')
+  if ($btnRight.innerText === 'SPLIT') {
+    printNewSplit()
+  }
+  else {
+    chronometer.resetClick()
+    printTime() // To print "00:00"
+    clearSplits()
+  }
+};
